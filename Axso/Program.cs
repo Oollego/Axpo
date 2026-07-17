9aefcc792226ecac307dd8cc1706f44b12389d1b
Oollego <144150105+Oollego@users.noreply.github.com>
2026-07-17 23:42:38 +0200

    chore: create project structure

--- Axso/Program.cs
+++ Axso/Program.cs
@@ -5,7 +5,6 @@ namespace Axso
         public static void Main(string[] args)
         {
             var builder = Host.CreateApplicationBuilder(args);
-            builder.Services.AddHostedService<Worker>();
 
             var host = builder.Build();
             host.Run();
